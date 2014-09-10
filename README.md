fe03c987a13d8f7b8ef20bfddb57ef4ce3739b19
madanvkumarsjsu <madankumar.vijayakumar@sjsu.edu>
2014-09-10 11:53:58 -0700

    Change1
    Basic Change

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-Hello-Scala
-===========
+HelloScala
+==========
+
+Hello World Spring Boot Scala Web App
+
+
+How to run
+==========
+
+```sh
+gradle build
+gradle bootRun
+```
+
+
+
---
+++ src/main/scala/hello/HelloConfig.scala
@@ -0,0 +1,26 @@
+package hello
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation._
+
+/**
+ * This config class will trigger Spring @annotation scanning and auto configure Spring context.
+ *
+ * @author saung
+ * @since 1.0
+ */ 
+@Controller
+@EnableAutoConfiguration
+class HelloConfig {
+	
+	@RequestMapping(Array("/"))
+    @ResponseBody
+    def home(model: Model) {
+		return "Hello, scala"
+	}
+}
